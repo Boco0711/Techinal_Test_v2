1333a65aef130590709ceaef1affa5278f15176a
Boco0711 <s.lep.0711@gmail.com>
2021-06-28 11:57:03 +0200

    Now FragmentUserProfile Handle menu to go back to list, Added a cancel button still need to make it work, Data of user are updated with an empty field management

--- app/src/main/java/com/leprincesylvain/altentest/techinaltestv2/view/FragmentUserProfile.kt
+++ app/src/main/java/com/leprincesylvain/altentest/techinaltestv2/view/FragmentUserProfile.kt
@@ -3,12 +3,13 @@ package com.leprincesylvain.altentest.techinaltestv2.view
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.TextUtils
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.leprincesylvain.altentest.techinaltestv2.R
 import com.leprincesylvain.altentest.techinaltestv2.model.user.Address
 import com.leprincesylvain.altentest.techinaltestv2.model.user.UserTableModel
@@ -27,12 +28,15 @@ class FragmentUserProfile : Fragment() {
     private lateinit var preferences: SharedPreferences
     private lateinit var user: UserTableModel
     private var callToReadJson = 0
+    private var i = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_profile_layout, container, false)
     }
 
@@ -62,44 +66,109 @@ class FragmentUserProfile : Fragment() {
             }
         })
 
-        var i = 0
+        listenToCancel()
         profile_edit_button.setOnClickListener {
             i++
             i %= 2
             if (i == 0) {
-                user_firstname_text.isEnabled = false
-                user.firstName = user_firstname_text.text.toString()
-                user_lastname_text.isEnabled = false
-                user.lastName = user_lastname_text.text.toString()
-                user_city_text.isEnabled = false
-                user.address.city = user_city_text.text.toString()
-                user_postalcode_text.isEnabled = false
-// TODO handle error case                user.address.postalCode = Integer.parseInt(user_postalcode_text.text.toString())
-                user_street_text.isEnabled = false
-                user.address.street = user_street_text.text.toString()
-                user_streetcode_text.isEnabled = false
-                user.address.streetCode = user_streetcode_text.text.toString()
-                user_country_text.isEnabled = false
-                user.address.country = user_country_text.text.toString()
-                user_birthdate_text.isEnabled = false
-                // TODO handle error case  user.birthDate = user_birthdate_text.date
-                profile_edit_button.setText("Edit profile")
-                userViewModel.updateUser(this.requireContext(), user)
+                if (checkIfFieldsAreEmpty()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "One or more field are empty, please fill them in or click on cancel",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    i++
+                } else {
+                    changeCancelVisibility(false)
+                    changeEditText(false)
+                    updateUser()
+                    profile_edit_button.setText("Edit profile")
+                }
             } else {
-                user_firstname_text.isEnabled = true
-                user_lastname_text.isEnabled = true
-                user_city_text.isEnabled = true
-                user_postalcode_text.isEnabled = true
-                user_street_text.isEnabled = true
-                user_streetcode_text.isEnabled = true
-                user_country_text.isEnabled = true
-                user_birthdate_text.isEnabled = true
+                changeCancelVisibility(true)
+                changeEditText(true)
                 profile_edit_button.setText("Save Change")
             }
         }
     }
 
-    fun getDate(milliSeconds: Long, dateFormat: String?): String? {
+    private fun listenToCancel() {
+        profile_cancel_button.setOnClickListener {
+            userViewModel.updateUser(this.requireContext(), user)
+        }
+    }
+
+    private fun updateUser() {
+        user.firstName = user_firstname_text.text.toString()
+        user.lastName = user_lastname_text.text.toString()
+        user.address.city = user_city_text.text.toString()
+// TODO handle error case                user.address.postalCode = Integer.parseInt(user_postalcode_text.text.toString())
+        user.address.street = user_street_text.text.toString()
+        user.address.streetCode = user_streetcode_text.text.toString()
+        user.address.country = user_country_text.text.toString()
+        // TODO handle error case  user.birthDate = user_birthdate_text.date
+        userViewModel.updateUser(this.requireContext(), user)
+    }
+
+    private fun changeEditText(b: Boolean) {
+        user_firstname_text.isEnabled = b
+        user_lastname_text.isEnabled = b
+        user_city_text.isEnabled = b
+        user_postalcode_text.isEnabled = b
+        user_street_text.isEnabled = b
+        user_streetcode_text.isEnabled = b
+        user_country_text.isEnabled = b
+        user_birthdate_text.isEnabled = b
+    }
+
+    private fun changeCancelVisibility(boolean: Boolean) {
+        if (boolean)
+            profile_cancel_button.visibility = View.VISIBLE
+        else
+            profile_cancel_button.visibility = View.GONE
+
+    }
+
+    private fun checkIfFieldsAreEmpty(): Boolean {
+        when {
+            TextUtils.isEmpty(user_firstname_text.text) -> {
+                user_firstname_text.error = "You must declare a firstName"
+
+                return true
+            }
+            TextUtils.isEmpty(user_lastname_text.text) -> {
+                user_lastname_text.error = "You must declare a lastName"
+                return true
+            }
+            TextUtils.isEmpty(user_city_text.text) -> {
+                user_city_text.error = "You must declare a city"
+                return true
+            }
+            TextUtils.isEmpty(user_postalcode_text.text) -> {
+                user_postalcode_text.error = "You must declare a postalCode"
+                return true
+            }
+            TextUtils.isEmpty(user_street_text.text) -> {
+                user_street_text.error = "You must declare a street"
+                return true
+            }
+            TextUtils.isEmpty(user_streetcode_text.text) -> {
+                user_streetcode_text.error = "You must declare a streetCode"
+                return true
+            }
+            TextUtils.isEmpty(user_country_text.text) -> {
+                user_country_text.error = "You must declare a country"
+                return true
+            }
+            TextUtils.isEmpty(user_birthdate_text.text) -> {
+                user_birthdate_text.error = "You must declare a birthDate"
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun getDate(milliSeconds: Long, dateFormat: String?): String? {
         // Create a DateFormatter object for displaying date in specified format.
         val formatter = SimpleDateFormat(dateFormat)
 
@@ -132,6 +201,21 @@ class FragmentUserProfile : Fragment() {
             userViewModel.insertUser(this.requireContext(), userTableModel)
         } catch (e: IOException) {
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.profile_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_list -> {
+                Navigation.findNavController(this.requireView())
+                    .navigate(R.id.action_profileFragment_to_devicesFragment)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
